d7e7f17b09d5b61b5005bede9b7c98c6d5c95f9b
Ninhache <mr.carambar27@gmail.com>
2024-07-25 17:56:43 +0200

    feat: update to use the right tool

--- app/_components/ImageWithInfo.tsx
+++ app/_components/ImageWithInfo.tsx
@@ -1,13 +1,20 @@
 import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { cn } from "@/lib/utils";
+
+import { formatDateToDayMonth } from "@/lib/date";
+import Image from "next/image";
+import { Icons } from "./icons";
+import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
-import { cn } from "@/lib/utils";
-
-import Image from "next/image";
-import { Icons } from "./icons";
-import { formatDateToDayMonth } from "@/lib/date";
 
 type ImageWithInfoProps = {
   static: StaticImageData;
@@ -27,7 +34,7 @@ export interface StaticImageData {
   blurHeight?: number;
 }
 
-export const ImageWithInfo = (props: ImageWithInfoProps) => (
+export const ImageWithTooltip = (props: ImageWithInfoProps) => (
   <div className="relative flex flex-col items-end justify-end gap-2">
     <Image
       src={props.static.src}
@@ -37,26 +44,28 @@ export const ImageWithInfo = (props: ImageWithInfoProps) => (
       className={cn(props.className)}
     />
 
-    <div className="absolute bottom-2 right-2">
-      <HoverCard>
-        <HoverCardTrigger asChild>
-          <Icons.informations className="border-1 rounded-xl bg-black" />
-        </HoverCardTrigger>
-        <HoverCardContent align="end" className="w-80">
-          <div className="flex justify-between space-x-4">
-            <div className="space-y-1">
-              <p className="text-sm">{props.alt}</p>
-              {props.date && (
-                <div className="flex items-center justify-end pt-2">
-                  <span className="text-xs text-muted-foreground">
-                    {formatDateToDayMonth(props.date)}
-                  </span>
-                </div>
-              )}
+    <div className="absolute bottom-2 right-2 z-50">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Icons.informations className="border-1 cursor-pointer rounded-xl bg-black" />
+          </TooltipTrigger>
+          <TooltipContent align="end">
+            <div className="flex justify-between space-x-4">
+              <div className="space-y-1">
+                <p className="text-sm">{props.alt}</p>
+                {props.date && (
+                  <div className="flex items-center justify-end pt-2">
+                    <span className="text-xs text-muted-foreground">
+                      {formatDateToDayMonth(props.date)}
+                    </span>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        </HoverCardContent>
-      </HoverCard>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   </div>
 );
